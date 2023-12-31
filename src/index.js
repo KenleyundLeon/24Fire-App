2d2b9ab13dbfe588294f6ac9a9a5fe619cb82e6c
Kenley Dev <92249212+KenleyundLeon@users.noreply.github.com>
2023-12-31 10:29:48 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,69 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, Tray, BrowserWindow, Menu, nativeImage } = require('electron');
 const path = require('path');
 
+let win;
 function createWindow() {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 900,
     height: 600,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
   });
-  win.setTitle('Made with 💖 by Kenley. (if you like it spend me: https://paypal.me/kenleydev )')
-  win.setIcon(path.join(__dirname, 'assets', 'icon.png'));
   win.loadURL('https://24fire.de');
+  win.setBackgroundColor('rgb(17, 24, 39)')
+  win.setTitle('Made with 💖 by Kenley. (if you like it spend me: https://paypal.me/kenleydev)')
+  win.setIcon(path.join(__dirname, 'assets', 'icon.png'));
 }
 
 Menu.setApplicationMenu(null);
 
 app.on('ready', () => {
+  const icon = nativeImage.createFromPath(path.join(__dirname, 'assets', 'icon.png'));
+  tray = new Tray(icon);
+  tray.setToolTip('24fire.de');
+  tray.setTitle('Made with 💖 by Kenley.');
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Hide',
+      type: 'normal',
+      icon: path.join(__dirname, 'assets', 'Hide.png'),
+      click: () => {
+        const isVisible = win && win.isVisible();
+        if (isVisible) {
+          win.hide();
+        } else {
+          win.show();
+        }
+      }
+    },
+    {
+      label: 'Help',
+      type: 'normal',
+      icon: path.join(__dirname, 'assets', 'Help.png'),
+      click: () => {
+        win.loadURL('https://discord.gg/24fire');
+      }
+    },
+    {
+      label: 'Reload',
+      type: 'normal',
+      icon: path.join(__dirname, 'assets', 'Reload.png'),
+      click: () => {
+        win.reload();
+      }
+    },
+    {
+      label: 'Close',
+      type: 'normal',
+      icon: path.join(__dirname, 'assets', 'Close.png'),
+      click: () => {
+        win.close();
+      }
+    },
+  ]);
+  
+  tray.setContextMenu(contextMenu);
   createWindow();
 
   app.on('activate', () => {
